7080d76cf1490b642bc8685cd5481ddaed5f4561
tobi <tobias.schoch@vtxmail.ch>
2014-03-13 20:50:52 +0100

    changed script to process single file

--- SecuredPDF2txt.sh
+++ SecuredPDF2txt.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-for file in $1/Januar.pdf ; do
-	echo "process $file..."
-	filestripped="$2/${file%.pdf}"
- 	convert -density 300 $file "${filestripped}.png"
-	textfile="${filestripped}.txt"
-	touch $textfile
-	echo ''>$textfile
-	i=0
-	for pngfile in $2/${filestripped}*.png ; do
-		i=$(($i+1))
-		echo "read text from page $i..."
-		tesseract $pngfile ${pngfile%.png} -psm 3 -l deu
-		temptextfile="${pngfile%.png}.txt"
-		cat $textfile $temptextfile > temp
-		cat temp > $textfile
-		rm temp
-		rm $temptextfile
-		rm $pngfile
-	echo "write $textfile..."
-	done
-done	
+trunk="${1%.pdf}"
+echo $trunk
+convert -density 300 $1 "$trunk.png"
+textfile="$trunk.txt"
+touch $textfile
+echo ''>$textfile
+i=0
+for pngfile in $trunk*.png ; do
+	i1=$(($i+1))
+	echo "read text from page $i1..."
+	tesseract $pngfile temp -psm 3 -l deu
+	cat $textfile temp.txt > temp
+	cat temp > $textfile
+	rm temp
+	rm temp.txt
+done
+rm $trunk*.png
+
