4dfcb88b0811588ac0652c3645aeb2842cb2a457
tobi <tobias.schoch@vtxmail.ch>
2014-07-13 20:07:16 +0200

    improved table in report and the income/expense handling

--- dataplot.py
+++ dataplot.py
@@ -57,11 +57,13 @@ def calculate_statistics(data, start=None, stop=None, months=12, budget=None):
 
         budget = rec_join('Kategorie',Sum,budget)
 
-        budget = rec_append_fields(budget,'Differenz',np.abs(budget.Summe)-np.abs(budget.BudgetPeriode))
-        budget = rec_append_fields(budget,'RelativeDifferenz',budget.Differenz/np.abs(budget.BudgetPeriode))
-        budget = rec_append_fields(budget,'TeilVomJahresbudget',np.abs(budget.Summe)/np.abs(budget.Jahresbudget))
 
-        I2=np.argsort(np.abs(budget['Summe']))[::-1]
+        budget = rec_append_fields(budget,'GutSchlecht',np.logical_or((budget.Summe-budget.BudgetPeriode)>=0,np.allclose(budget.Summe,budget.BudgetPeriode,atol=0.5)))
+        budget = rec_append_fields(budget,'RelativeDifferenz',(budget.Summe-budget.BudgetPeriode)/budget.BudgetPeriode)
+        budget = rec_append_fields(budget,'Differenz',(budget.Summe-budget.BudgetPeriode)*np.sign(budget['Jahresbudget']))
+        budget = rec_append_fields(budget,'TeilVomJahresbudget',budget.Summe/budget.Jahresbudget)
+
+        I2=np.lexsort((-budget['Summe'],budget['Jahresbudget']>0))[::-1]
         budget = budget[I2]
 
         return rec, budget
@@ -81,24 +83,40 @@ def plot_category(category,data,budget=None):
     if budget is not None:
         bud = float(budget['Jahresbudget'][budget.Kategorie==category][0])/12.
     fp = FontProperties(size='medium')
-    if avg > 0:
+
+    # Ausgaben
+    if budget['Jahresbudget'][budget.Kategorie==category] < 0:
+
         linecolor='#5B9ECF'
         plt.plot(category_data.Datum,category_data.Total,linewidth=2,color=linecolor,marker='o')
+
         plt.fill_between(category_data.Datum,category_data.Total,color=linecolor,alpha=0.6)
+
         pavg, =plt.plot(category_data.Datum,np.repeat(avg,len(category_data)),color='r',alpha=0.4,linewidth=2,linestyle='--')
+
         plt.title('Ausgaben '+category)
         if budget is not None:
-            pbud, =plt.plot(category_data.Datum,np.repeat(bud,len(category_data)),color='g',alpha=0.4,linewidth=2,linestyle='--')
-            plt.legend([pavg,pbud],['Durchschnitt {0:.0f} CHF'.format(avg),'Budget {0:.0f} CHF'.format(bud)],prop=fp)
+
+            pbud, =plt.plot(category_data.Datum,np.repeat(-bud,len(category_data)),color='g',alpha=0.4,linewidth=2,linestyle='--')
+            plt.legend([pavg,pbud],['Durchschnitt {0:.0f} CHF'.format(avg),'Budget {0:.0f} CHF'.format(-bud)],prop=fp)
         else:
             plt.legend([pavg],['Durchschnitt {0:.0f} CHF'.format(avg)],prop=fp)
+
+    # Einkommen
     else:
+
         linecolor='#95D444'
-        plt.plot(category_data.Datum,category_data.Total,linewidth=2,color=linecolor,marker='o')
+
+        plt.plot(category_data.Datum,-category_data.Total,linewidth=2,color=linecolor,marker='o')
+
         plt.fill_between(category_data.Datum,-category_data.Total,color=linecolor,alpha=0.4)
+
         pavg, =plt.plot(category_data.Datum,-np.repeat(avg,len(category_data)),color='r',alpha=0.4,linewidth=2,linestyle='--')
+
         plt.title('Einkommen '+category)
+
         if budget is not None:
+
             pbud, =plt.plot(category_data.Datum,np.repeat(bud,len(category_data)),color='g',alpha=0.4,linewidth=2,linestyle='--')
             plt.legend([pavg,pbud],['Durchschnitt {0:.0f} CHF'.format(-avg),'Budget {0:.0f} CHF'.format(bud)],prop=fp)
         else:
@@ -114,7 +132,10 @@ def plot_category(category,data,budget=None):
     plt.ylabel('Total Monat [CHF]')
 
     ylim = plt.ylim()
-    ylim = (0,ylim[1]*1.2)
+    if ylim[0] < 0:
+        ylim = (ylim[0]*1.2,ylim[1]*1.2)
+    else:
+        ylim = (0,ylim[1]*1.2)
     plt.ylim(ylim)
 
     return fh
@@ -133,24 +154,71 @@ def create_report(start,data,budget,stop=datetime.today(),output='figures/report
         plt.xlim((0,1))
         plt.ylim((0,1))
         text = u"Report MyFinance\n\n"
-        text+= u"Periode: \n"
         text+= start.strftime('%d. %B %Y').decode('utf-8')
         text+= " bis "
         text+= stop.strftime(u'%d. %B %Y').decode('utf-8')
-        table = []
+        dtable = []
+        columns = ['Kategorie','Total\n(in Periode)','Budget\n(in Periode)','Differenz\n(bez. Budget)','Abweichung\n(relativ)','% Jahresbudget']
         for row in budget:
             eff = row['Summe']
             bud_year = row['Jahresbudget']
             bud_prop = row['BudgetPeriode']
 
-            table.append([row['Kategorie'],"{0:d}".format(int(eff)),"{0:d}".format(int(bud_prop)),
+            dtable.append([row['Kategorie'],"{0:d}".format(int(eff)),"{0:d}".format(int(bud_prop)),
                           "{0:d}".format(int(row['Differenz'])),"{0:.0%}".format(row['RelativeDifferenz']),
                           "{0:.0%}".format(row['TeilVomJahresbudget'])])
-        #table.append(['TOTAL:',"{0:.0f}".format(sum(budget['Summe'])),"{0:0.f}".format(sum(budget['BudgetPeriode'])),"-","-","-"])
-        columns = ['Kategorie','Total','Budget','Differenz','Abweichung','% Jahresbudget']
+
+
+        dtable.append([""]*len(columns))
+        bilanz = sum(budget['Summe'])
+        dtable.append(["Einkommen:","{0:.0f}".format(sum(budget['Summe'][budget['Summe']>0])),"Ausgaben:","{0:.0f}".format(sum(budget['Summe'][budget['Summe']<=0])),"Bilanz:","{0:.0f}".format(bilanz)])
         # colwidths = [.25,0.2,0.2,0.2,0.2,0.2]
-        plt.table(cellText=table,colLabels=columns,loc="lower center",cellLoc='center')#,colWidths=colwidths)
-        plt.text(0.5,0.9,text,transform=plt.gca().transAxes,horizontalalignment='center')
+        table=plt.table(cellText=dtable,colLabels=columns,loc="lower center",cellLoc='center')#,colWidths=colwidths)
+        plt.text(0.5,0.97,text,transform=plt.gca().transAxes,horizontalalignment='center',verticalalignment="bottom")
+
+        ## change cell properties
+        cells = table.get_celld()
+
+        headercolor = "grey" #"#d3d7cf"
+        goodcolor = "yellow"#"#FDFD96"
+        badcolor = "red"#"#d75040"
+        for i in range(len(columns)):
+            cell = cells[0,i]
+            cell.set_facecolor(headercolor)
+            cell.set_alpha(0.4)
+            cell.set_height(0.07)
+            cell.set_lw(0.1)
+            cell.set_fontsize(15)
+
+        for j in range(1,len(budget)+1):
+
+            if budget['GutSchlecht'][j-1]:
+                color = goodcolor
+            else:
+                color = badcolor
+
+            for i in range(len(columns)):
+                cell = cells[j,i]
+                cell.set_facecolor(color)
+                cell.set_alpha(0.4)
+                cell.set_lw(0.3)
+
+        for i in range(len(columns)):
+            cell = cells[len(dtable)-1,i]
+            cell.set_lw(0)
+
+        for i in range(len(columns)):
+            cell = cells[len(dtable),i]
+            if i%2==0:
+                cell.set_facecolor(headercolor)
+            else:
+                cell.set_facecolor(goodcolor)
+            if i==len(columns)-1 and bilanz<0:
+                cell.set_facecolor(badcolor)
+
+            cell.set_alpha(0.4)
+            cell.set_lw(0.3)
+
         pdf.savefig()
 
         # make category plots
