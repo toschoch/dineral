93ccb0a2fffec1b18e1296655a2ef2dd61cb0c51
Tobias Schoch <tobias.schoch@vtxmail.ch>
2018-01-10 23:44:06 +0100

    fixed parser mastercard extract entry regex

--- dineral/dataplugins/mastercard.py
+++ dineral/dataplugins/mastercard.py
@@ -135,7 +135,7 @@ class MasterCard(DataPlugin):
         lines = map(unicode, lines)
 
 
-        newentry = re.compile("([0-9]{2,2}.[0-9]{2,2}.[0-9]{2,2}) ([\w\s-]+) ([A-Z]{3,3} [0-9]+.[0-9]{2,2})? \W* ([0-9]+.[0-9]{2,2})[\s0-9.]*",re.UNICODE)
+        newentry = re.compile("([0-9]{2,2}.[0-9]{2,2}.[0-9]{2,2}) ([\D\s-]+) ([A-Z]{3,3} [0-9]+.[0-9]{2,2})? \W* ([0-9]+.[0-9]{2,2})[\s0-9.]*",re.UNICODE)
 
         table = []
         it = iter(lines)
